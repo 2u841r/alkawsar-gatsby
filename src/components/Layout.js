e7c27d6b1d977f0c95c74ab2a0d53e268eb24af6
Zubair <zizdjbd@gmail.com>
2024-09-11 19:03:59 +0600

    button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,21 +3,47 @@ import Navbar from './Navbar'
 import '../styles/global.css'
 
 const Layout = ({ children }) => {
-    return (
-        <div className="flex flex-col min-h-screen w-full px-8 box-border max-w-7xl mx-auto">
-            <Navbar />
-            <div className="flex-grow mt-8 mb-8 md:mr-8 md:ml-24">
-                {children}
-            </div>
-            <footer className="mt-auto text-center py-5">
-                <div>
-                    <p> কপিরাইট © ২০২৪ মাসিক আলকাউসার</p>
-                    <p> Developer by <a className='text-green-400' href='https://zubairiz.com' target='_blank' rel="noreferrer"> Zubair Ibn Zamir </a> - <a className='text-green-500' href='https://zmt3.com' rel="noreferrer"> ZMT3 </a> </p>
-                    <a className='text-blue-400' href='https://github.com/2u841r/alkawsar-gatsby' target='_blank' rel="noreferrer">source code on gitbub</a>
-                </div>
-            </footer>
+  return (
+    <div className="flex flex-col min-h-screen w-full px-8 box-border max-w-7xl mx-auto">
+      <Navbar />
+      <div className="flex-grow mt-8 mb-8">
+        {children}
+      </div>
+      <footer className="mt-auto text-center py-5">
+        <div>
+          <p> কপিরাইট © ২০২৪ মাসিক আলকাউসার</p>
+          <p>
+            Developer by{' '}
+            <a
+              className="text-green-400"
+              href="https://zubairiz.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Zubair Ibn Zamir
+            </a>{' '}
+            -{' '}
+            <a
+              className="text-green-500"
+              href="https://zmt3.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              ZMT3
+            </a>
+          </p>
+          <a
+            className="text-blue-400"
+            href="https://github.com/2u841r/alkawsar-gatsby"
+            target="_blank"
+            rel="noreferrer"
+          >
+            source code on GitHub
+          </a>
         </div>
-    )
+      </footer>
+    </div>
+  )
 }
 
 export default Layout
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,95 @@
 import { Link, graphql, useStaticQuery } from 'gatsby'
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-    const data = useStaticQuery(graphql`
-      query MyQuery1 {
-        site {
-            siteMetadata {
-                title
-                }
-            }
+  const data = useStaticQuery(graphql`
+    query MyQuery1 {
+      site {
+        siteMetadata {
+          title
         }
-        `)
+      }
+    }
+  `)
 
-    const { title } = data.site.siteMetadata;
+  const { title } = data.site.siteMetadata
 
-    return (
-        <nav className="flex justify-between items-center my-5 mx-auto md:grid md:grid-cols-2 md:my-10">
-        <h1 className="hover:border-transparent">
-            <Link to='/' className="inline-block">
-                <img src='/alkawsar_logo.svg' width='100' alt={title} />
-            </Link>
-        </h1>
-        <div className="text-right">
-            <Link to='/articles' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">সব প্রবন্ধ</Link>
-            <Link to='/issues' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">সকল সংখ্যা</Link>
-            <Link to='/sections' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">বিভাগ</Link>
-            <Link to='/topics' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">বিষয়বস্তু</Link>
-            <Link to='/authors' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">লেখক</Link>
-            <Link to='/about' className="ml-2.5 md:ml-5 font-normal pb-2 border-b-3 border-transparent hover:border-white">পরিচিতি</Link>
-        </div>
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
+  return (
+    <nav className="flex justify-between items-center my-5 mx-auto w-full max-w-7xl md:my-10">
+      {/* Logo on the left */}
+      <div className="flex-shrink-0">
+        <Link to="/" className="inline-block">
+          <img src="/alkawsar_logo.svg" width="100" alt={title} />
+        </Link>
+      </div>
+
+      {/* Hamburger Button for mobile */}
+      <button
+        onClick={toggleMenu}
+        className="block md:hidden"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+
+      {/* Menu items on the right */}
+      <div className="hidden md:flex items-center space-x-5">
+        <Link to="/articles" className="hover:border-white">সব প্রবন্ধ</Link>
+        <Link to="/issues" className="hover:border-white">সকল সংখ্যা</Link>
+        <Link to="/sections" className="hover:border-white">বিভাগ</Link>
+        <Link to="/topics" className="hover:border-white">বিষয়বস্তু</Link>
+        <Link to="/authors" className="hover:border-white">লেখক</Link>
+        <Link to="/about" className="hover:border-white">পরিচিতি</Link>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`absolute top-0 right-0 h-full bg-purple-700 text-white flex flex-col items-center justify-center w-full transition-transform ${
+          isOpen ? 'translate-x-0' : 'translate-x-full'
+        } md:hidden`}
+      >
+        <Link to="/" className="lg:hidden p-4" onClick={toggleMenu}>
+          হোমপেজ
+        </Link>
+        <Link to="/articles" className="p-4" onClick={toggleMenu}>
+          সব প্রবন্ধ
+        </Link>
+        <Link to="/issues" className="p-4" onClick={toggleMenu}>
+          সকল সংখ্যা
+        </Link>
+        <Link to="/sections" className="p-4" onClick={toggleMenu}>
+          বিভাগ
+        </Link>
+        <Link to="/topics" className="p-4" onClick={toggleMenu}>
+          বিষয়বস্তু
+        </Link>
+        <Link to="/authors" className="p-4" onClick={toggleMenu}>
+          লেখক
+        </Link>
+        <Link to="/about" className="p-4" onClick={toggleMenu}>
+          পরিচিতি
+        </Link>
+      </div>
     </nav>
-    )
+  )
 }
 
 export default Navbar
