72ab9f9eab61b965669e577d63a6b128b16cb431
Zubair <zizdjbd@gmail.com>
2024-09-13 16:13:41 +0600

    logo-link-fixed

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,7 +1,7 @@
 const config = {
     siteTitle: "আল কাউসার",
     siteUrl: 'https://alkawsar.zmt3.com',
-    siteLogo: '/alkawsar_logo.png',
+    siteLogo: '/alkawsar_logo.svg',
     description:
       'গবেষণামূলক উচ্চতর শিক্ষাপ্রতিষ্ঠান মারকাযুদ দাওয়াহ আলইসলামিয়া ঢাকা এর মুখপত্র',
   }
