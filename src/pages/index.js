cf4549f5f0810d8a322991282ccbd7117e0aa0a1
Zubair <zizdjbd@gmail.com>
2024-09-14 14:35:52 +0600

    lasttry

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,11 +15,11 @@ const Index = ({ data }) => {
       <Helmet title={`মূলপাতা | ${config.siteTitle}`} />
       <SEO />
       <h2 className="text-3xl font-bold text-center my-2">সেপ্টেম্বর ২০২৪</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 grid-flow-row-dense">
         {articles.map(a => {
           const { an, title, issue, author, category } = a.frontmatter;
           return (
-            <Link to={`/articles/${an}`} key={a.id} className="no-underline">
+            <Link to={`/articles/${an}`} key={a.id} className="no-underline min-w-[200px] flex-grow">
               <div className="bg-white bg-opacity-10 hover:bg-opacity-15 transition-colors duration-300 p-4 rounded-lg h-full flex flex-col justify-between">
                 <h5 className="text-sm overflow-hidden text-ellipsis whitespace-nowrap">
                   <Link to={`/issues/${issue}`} className="hover:underline">{issue}</Link> :&nbsp;
