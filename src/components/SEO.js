c1a89ba2e942e9fbbc8ab225cbf5f60313208108
Zubair <zizdjbd@gmail.com>
2024-09-15 18:52:14 +0600

    og

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -82,13 +82,14 @@ export const SEO = ({ postNode, postPath, postSEO, customDescription }) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:type" content="website" />
+      <meta property="og:image:height" content="960" />
+      <meta property="og:image:width" content="960" />
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
-      <meta property="og:type" content="website" />
-      <meta property="og:image:height" content="960" />
-      <meta property="og:image:width" content="960" />
+
     </Helmet>
   )
 }
